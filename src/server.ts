d25e84ffe5e7363776bc3f42c0f6271279c5c2d6
Noyon-Podder <noyon.vai369@gmail.com>
2023-11-21 00:15:18 +0600

    professional project setup

--- src/server.ts
+++ src/server.ts
@@ -7,7 +7,7 @@ async function main() {
   try {
     await mongoose.connect(config.database_url as string);
     app.listen(config.port, () => {
-      console.log(`Server Listening on port ${config.port}`);
+      console.log(`Server running on port ${config.port}`);
     });
   } catch (err) {
     console.log('To Database setup: ', err);
